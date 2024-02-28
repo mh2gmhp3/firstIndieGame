1773d86630f83ce5729d07fe4b18e9f859f9c4c9
wei_chen <winnie87020653@gmail.com>
2024-02-28 18:28:30 +0800

    feat: add GameSystemEnterGameFlowStepSettingEditor

---
+++ Assets/Script/Framework/GameSystem/BaseGameSystem/Editor/GameSystemEnterGameFlowStepSettingEditor.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UEditor = UnityEditor.Editor;
+using Framework.Logging;
+using Framework.GameSystem;
+using System;
+
+namespace Framework.Editor.GameSystem.EnterGameFlowStepSetting
+{
+    [CustomEditor(typeof(GameSystemEnterGameFlowStepSetting))]
+    public class GameSystemEnterGameFlowStepSettingEditor : UEditor
+    {
+        private GameSystemEnterGameFlowStepSetting _instance = null;
+
+        private HashSet<string> _stepConfigName = new HashSet<string>();
+        private Dictionary<int, StepConfig> _idToStepCongifDic = new Dictionary<int, StepConfig>();
+        private string _newStepConfigName = string.Empty;
+
+        private void OnEnable()
+        {
+            _instance = target as GameSystemEnterGameFlowStepSetting;
+            InitEdiotrSetting();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if (_instance == null)
+            {
+                Log.LogError("GameSystemEnterGameFlowStepSettingEditor _instance is null");
+                return;
+            }
+
+            DrawStepGUI();
+            EditorGUILayout.Space(10);
+            DrawStepConfigGUI();
+        }
+
+        private void InitEdiotrSetting()
+        {
+            RefreshStepConfigSetting();
+        }
+
+        private void RefreshStepConfigSetting()
+        {
+            if (_instance == null)
+                return;
+
+            _stepConfigName.Clear();
+            for (int i = 0; i < _instance.EnterGameFlowStepConfigs.Count; i++)
+            {
+                _stepConfigName.Add(_instance.EnterGameFlowStepConfigs[i].Name);
+            }
+
+            _instance.TryGetStepConfigDic(out _idToStepCongifDic);
+        }
+
+        private void AddNewStepConfig(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return;
+
+            name = name.Replace(" ", "");
+            if (_stepConfigName.Contains(name))
+                return;
+
+            if (!TryGetMaxStepConfigId(out int newId))
+                return;
+
+            _instance.EnterGameFlowStepConfigs.Add(
+                new StepConfig(newId, name));
+
+            EditorUtility.SetDirty(_instance);
+
+            RefreshStepConfigSetting();
+        }
+
+        private bool TryGetMaxStepConfigId(out int id)
+        {
+            id = 0;
+            if (_instance == null)
+                return false; ;
+
+            id = 0;
+            for (int i = 0; i < _instance.EnterGameFlowStepConfigs.Count; i++)
+            {
+                int oriId = _instance.EnterGameFlowStepConfigs[i].Id;
+                if (oriId > id)
+                    id = oriId;
+            }
+
+            id++;
+            return true;
+        }
+
+        List<int> _removeStepList = new List<int>();
+        private void DrawStepGUI()
+        {
+            for (int i = 0; i < _instance.EnterGameFlowStep.Count; i++)
+            {
+                var stepId= _instance.EnterGameFlowStep[i];
+                EditorGUILayout.BeginHorizontal(CommonGUIStyle.Default_Box);
+                {
+                    GUILayout.Label($"{stepId}", GUILayout.Width(50));
+                    if (_idToStepCongifDic.TryGetValue(stepId, out var stepConfig))
+                    {
+                        GUILayout.Label(stepConfig.Name);
+                    }
+                    GUILayout.FlexibleSpace();
+                    if (GUILayout.Button("X"))
+                    {
+                        _removeStepList.Add(stepId);
+                    }
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+
+            foreach (var removeStep in _removeStepList)
+            {
+                _instance.EnterGameFlowStep.Remove(removeStep);
+            }
+            if (_removeStepList.Count > 0)
+                _removeStepList.Clear();
+        }
+
+        #region DrawStepConfigGUI
+
+        private void DrawStepConfigGUI()
+        {
+            EditorGUILayout.BeginVertical(CommonGUIStyle.Default_Box);
+            {
+                DrawStepConfig_NewStepConfigGUI();
+
+                EditorGUILayout.Space(10);
+
+                DrawStepConfig_StepConfigListGUI();
+            }
+            EditorGUILayout.EndVertical();
+        }
+
+        private void DrawStepConfig_NewStepConfigGUI()
+        {
+            EditorGUILayout.BeginHorizontal(CommonGUIStyle.Default_Box);
+            {
+                GUILayout.Label("新增階段名稱");
+                _newStepConfigName = GUILayout.TextField(_newStepConfigName);
+                if (GUILayout.Button("新增"))
+                {
+                    AddNewStepConfig(_newStepConfigName);
+                }
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        List<StepConfig> _removeStepConfigList = new List<StepConfig>();
+        private void DrawStepConfig_StepConfigListGUI()
+        {
+            for (int i = 0; i < _instance.EnterGameFlowStepConfigs.Count; i++)
+            {
+                var stepConfig = _instance.EnterGameFlowStepConfigs[i];
+                EditorGUILayout.BeginHorizontal(CommonGUIStyle.Default_Box);
+                {
+                    GUILayout.Label($"{stepConfig.Id}", GUILayout.Width(50));
+                    GUILayout.Label(stepConfig.Name);
+                    GUILayout.FlexibleSpace();
+                    if (GUILayout.Button("X"))
+                    {
+                        _removeStepConfigList.Add(stepConfig);
+                    }
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+
+            foreach (var removeStepConfig in _removeStepConfigList)
+            {
+                _instance.EnterGameFlowStepConfigs.Remove(removeStepConfig);
+            }
+            if (_removeStepConfigList.Count > 0)
+            {
+                RefreshStepConfigSetting();
+                _removeStepConfigList.Clear();
+            }
+        }
+
+        #endregion
+    }
+}
--- Assets/Script/Framework/GameSystem/BaseGameSystem/GameSystemEnterGameFlowStepSetting.cs
+++ Assets/Script/Framework/GameSystem/BaseGameSystem/GameSystemEnterGameFlowStepSetting.cs
@@ -7,6 +7,30 @@ using System.IO;
 
 namespace Framework.GameSystem
 {
+#if UNITY_EDITOR
+    /// <summary>
+    /// 階段設定
+    /// </summary>
+    [Serializable]
+    public class StepConfig
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id;
+        /// <summary>
+        /// 階段名稱
+        /// </summary>
+        public string Name;
+
+        public StepConfig(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+#endif
+
     [CreateAssetMenu(fileName = "GameSystemEnterGameFlowStepSetting",  menuName = "GameSystem/GameSystemEnterGameFlowStepSetting")]
     public class GameSystemEnterGameFlowStepSetting : ScriptableObject
     {
@@ -16,5 +40,27 @@ namespace Framework.GameSystem
         public static string RESOURCE_PATH = "Setting/GameSystem/GameSystemEnterGameFlowStepSetting";
 
         public List<int> EnterGameFlowStep = new List<int>();
+
+#if UNITY_EDITOR
+        public List<StepConfig> EnterGameFlowStepConfigs = new List<StepConfig>();
+
+        /// <summary>
+        /// 嘗試獲取階段設定Dic
+        /// </summary>
+        /// <param name="idToConfigDic"></param>
+        /// <returns>具有相同Id時回傳false</returns>
+        public bool TryGetStepConfigDic(out Dictionary<int, StepConfig> idToConfigDic)
+        {
+            idToConfigDic = new Dictionary<int, StepConfig>();
+            foreach (var stepConfig in EnterGameFlowStepConfigs)
+            {
+                if (idToConfigDic.ContainsKey(stepConfig.Id))
+                    return false;
+
+                idToConfigDic.Add(stepConfig.Id, stepConfig);
+            }
+            return true;
+        }
+#endif
     }
 }
