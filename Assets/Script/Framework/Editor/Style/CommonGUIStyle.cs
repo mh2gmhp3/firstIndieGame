36fba207dc3ea26619c6fbd0adb5db57751d552d
wei_chen <winnie87020653@gmail.com>
2024-02-28 18:28:03 +0800

    feat: add CommonGUIStyle

---
+++ Assets/Script/Framework/Editor/Style/CommonGUIStyle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework.Editor
+{
+    public class CommonGUIStyle
+    {
+        public static string Default_Box = "Box";
+    }
+}
