3e24253034bf203f00a7831099994368b5dabad6
wei_chen <winnie87020653@gmail.com>
2024-04-13 13:00:59 +0800

    feat add GameState

---
+++ Assets/Script/Game/GameMainSystem/GameMainSystem.GameState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameMainSystem
+{
+    public partial class GameMainSystem
+    {
+        public enum GameState
+        {
+            None,
+            UI,
+            Normal,
+        }
+
+        private GameState _gameState = GameState.None;
+    }
+}
