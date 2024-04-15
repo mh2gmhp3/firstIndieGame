cb0ce9cf42c277199a89261d17f7c02676e0f363
wei_chen <winnie87020653@gmail.com>
2024-04-15 17:06:47 +0800

    feat: GameThreeDimensionalCharacterController add summary

--- Assets/Script/Game/GameMainSystem/GameThreeDimensionalCharacterController.cs
+++ Assets/Script/Game/GameMainSystem/GameThreeDimensionalCharacterController.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 namespace GameMainSystem
 {
+    /// <summary>
+    /// 三維空間的角色控制器
+    /// </summary>
     public class GameThreeDimensionalCharacterController
     {
+        /// <summary>
+        /// 啟用
+        /// </summary>
         private bool _enable = false;
 
+        /// <summary>
+        /// 角色Transform
+        /// </summary>
         private Transform _characterTrans = null;
 
+        /// <summary>
+        /// 移動輸入軸
+        /// </summary>
         private Vector3 _moveAxis = Vector3.zero;
+        /// <summary>
+        /// 移動選轉方向
+        /// </summary>
         private Quaternion _moveQuaternion = Quaternion.identity;
 
         public void DoLateUpdate()
@@ -27,21 +42,37 @@ namespace GameMainSystem
             _characterTrans.position += moveForward * speed * Time.deltaTime;
         }
 
+        /// <summary>
+        /// 設定啟用 預設為關閉
+        /// </summary>
+        /// <param name="enable"></param>
         public void SetEnable(bool enable)
         {
             _enable = enable;
         }
 
+        /// <summary>
+        /// 設定角色Transform
+        /// </summary>
+        /// <param name="transform"></param>
         public void SetCharacterTransform(Transform transform)
         {
             _characterTrans = transform;
         }
 
+        /// <summary>
+        /// 設定移動輸入軸
+        /// </summary>
+        /// <param name="axis"></param>
         public void SetMoveAxis(Vector3 axis)
         {
             _moveAxis = axis;
         }
 
+        /// <summary>
+        /// 設定移動選轉方向
+        /// </summary>
+        /// <param name="quaternion"></param>
         public void SetMoveQuaternion(Quaternion quaternion)
         {
             //只取Y軸
