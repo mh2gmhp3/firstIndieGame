33aa7b01a81d61b4f9e4096fe3e4010032eba31f
wei_chen <winnie870206@gmail.com>
2024-05-09 20:03:54 +0800

    feat: add List TryGet extension

---
+++ Assets/Script/Framework/Extension/List.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Extension
+{
+    public static class ListExtension
+    {
+        public static bool TryGet<T>(this IList<T> list, int index, out T result)
+        {
+            result = default(T);
+            if (list == null)
+                return false;
+
+            if (index < 0 || index >= list.Count)
+                return false;
+
+            result = list[index];
+            return true;
+        }
+
+        public static bool TryGetFirst<T>(this IList<T> list, out T result)
+        {
+            return TryGet(list, 0, out result);
+        }
+    }
+}
