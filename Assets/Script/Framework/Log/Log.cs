9933a85c9978cb484399ec725238f72c31e44eea
wei_chen <winnie870206@gmail.com>
2024-06-22 21:44:14 +0800

    feat: Log can show caller method => TypeName.MethodName

--- Assets/Script/Framework/Log/Log.cs
+++ Assets/Script/Framework/Log/Log.cs
@@ -1,24 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Diagnostics;
+using Debug = UnityEngine.Debug;
 
 namespace Logging
 {
     public static class Log
     {
-        public static void LogInfo(object message)
+        public static void LogInfo(object message, bool withMethod = false)
         {
-            Debug.Log(message);
+            Debug.Log(GetMessage(message, withMethod));
         }
 
-        public static void LogWarning(object message)
+        public static void LogWarning(object message, bool withMethod = false)
         {
-            Debug.LogWarning(message);
+            Debug.LogWarning(GetMessage(message, withMethod));
         }
 
-        public static void LogError(object message)
+        public static void LogError(object message, bool withMethod = false)
         {
-            Debug.LogError(message);
+            Debug.LogError(GetMessage(message, withMethod));
+        }
+
+        private static string GetMessage(object message, bool withMethod)
+        {
+            string msg = message.ToString();
+            if (withMethod)
+                msg = GetCallerMethodName(3) + " " + msg;
+            //GetCallerMethodName => 0
+            //GetMessage => 1
+            //各Log接口 => 2
+            //呼叫的Method => 3 使用
+            return msg;
+        }
+
+        private static string GetCallerMethodName(int stackFrameIndex)
+        {
+            StackTrace st = new StackTrace();
+            StackFrame sf = st.GetFrame(stackFrameIndex);
+
+            var method = sf.GetMethod();
+            if (method == null)
+                return "Method is invalid";
+
+            return $"{method.DeclaringType.Name}.{method.Name}";
         }
     }
 }
