35c91f12a32408d0fab46d8bac6b943f1f21da56
wei_chen <winnie870206@gmail.com>
2024-07-10 12:32:05 +0800

    feat: add ObjectReference

---
+++ Assets/Script/Framework/ComponentUtility/ObjectReference/ObjectReference.cs
@@ -0,0 +1,42 @@
+﻿using Logging;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace ComponentUtility
+{
+    [Serializable]
+    public class ObjectReference
+    {
+        [SerializeField]
+        private List<Object> _objectList =
+            new List<Object>();
+
+        private Dictionary<string, Object> _nameToObjectDic =
+            new Dictionary<string, Object>();
+
+        public T GetObject<T>(string name) where T : Object
+        {
+            T resultObj = null;
+            if (!_nameToObjectDic.TryGetValue(name, out var obj))
+            {
+                Log.LogError($"Object not exist, name:{name}", true);
+                return null;
+            }
+
+            resultObj = obj as T;
+            if (resultObj == null)
+            {
+                Log.LogError(
+                    $"Object Type is invalid, " +
+                    $"name:{name}, useType:{typeof(T).Name}, " +
+                    $"registerType:{obj.GetType().Name}");
+                return null;
+            }
+
+            return resultObj;
+        }
+    }
+}
