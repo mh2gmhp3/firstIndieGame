38ae899d0b2775a27c73cb4b570f1e6ad671d790
wei_chen <winnie870206@gmail.com>
2024-10-20 12:26:53 +0800

    feat: add DragAndDropHandler

---
+++ Assets/Script/Framework/Editor/Utility/DragAndDropHandler.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Framework.Editor.Utility
+{
+    /// <summary>
+    /// 拖曳行為管理
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class DragAndDropHandler<T>
+    {
+        /// <summary>
+        /// 拖曳區域結構
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public struct DragAndDropArea<T>
+        {
+            public Rect Area;
+            public Action<T> Action;
+
+            public DragAndDropArea(Rect area, Action<T> action)
+            {
+                Area = area;
+                Action = action;
+            }
+        }
+
+        /// <summary>
+        /// 拖曳區域
+        /// </summary>
+        private List<DragAndDropArea<T>> _areaList = new List<DragAndDropArea<T>>();
+
+        /// <summary>
+        /// 清空拖曳區域
+        /// </summary>
+        public void ClearArea()
+        {
+            _areaList.Clear();
+        }
+
+        /// <summary>
+        /// 新增拖曳區域
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <param name="action"></param>
+        public void AddArea(Rect rect, Action<T> action)
+        {
+            _areaList.Add(new DragAndDropArea<T>(rect, action));
+        }
+
+        /// <summary>
+        /// 處裡拖曳行為
+        /// </summary>
+        public void ProcessDragAndDrop()
+        {
+            Event ev = Event.current;
+            switch (ev.type)
+            {
+                case EventType.DragUpdated:
+                case EventType.DragPerform:
+                    for (int i = 0; i < _areaList.Count; i++)
+                    {
+                        if (!_areaList[i].Area.Contains(ev.mousePosition))
+                            continue;
+
+                        DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+
+                        if (ev.type == EventType.DragPerform)
+                        {
+                            DragAndDrop.AcceptDrag();
+
+                            foreach (Object draggedObject in DragAndDrop.objectReferences)
+                            {
+                                if (draggedObject is T obj)
+                                {
+                                    _areaList[i].Action.Invoke(obj);
+                                }
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
