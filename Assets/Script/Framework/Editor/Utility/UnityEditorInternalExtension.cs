f6140636cddb25334c323630910e3d5c5f5e8828
wei_chen <winnie870206@gmail.com>
2024-10-27 21:00:45 +0800

    feat: add UnityEditorInternalExtension  AddComponentUncheckedUndoable

---
+++ Assets/Script/Framework/Editor/Utility/UnityEditorInternalExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace Framework.Editor.Utility
+{
+    public class UnityEditorInternalExtension
+    {
+        /// <summary>
+        /// 添加Component 無視檢查與回退
+        /// </summary>
+        /// <param name="go"></param>
+        /// <param name="obj"></param>
+        public static void AddComponentUncheckedUndoable(GameObject go, UnityEngine.Object obj)
+        {
+            MethodInfo addScriptMethod = typeof(InternalEditorUtility).GetMethod(
+               "AddScriptComponentUncheckedUndoable",
+               BindingFlags.Static | BindingFlags.NonPublic);
+            if (addScriptMethod != null)
+                addScriptMethod.Invoke(null, new object[] { go, obj });
+        }
+    }
+}
