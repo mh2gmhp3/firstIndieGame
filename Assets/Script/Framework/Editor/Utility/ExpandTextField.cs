6111382dc9e0aacf3b32900d408007ffca1fe8dd
wei_chen <winnie870206@gmail.com>
2024-10-27 21:00:31 +0800

    feat: add ExpandTextField

---
+++ Assets/Script/Framework/Editor/Utility/ExpandTextField.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Framework.Editor.Utility
+{
+    /// <summary>
+    /// 可擴展文字區域
+    /// </summary>
+    public class ExpandTextField
+    {
+        private bool _expand;
+        private string _text;
+
+        public string Text => _text;
+
+        /// <summary>
+        /// 觸發擴展
+        /// </summary>
+        public void TriggerExpand()
+        {
+            _expand = !_expand;
+        }
+
+        /// <summary>
+        /// 重設
+        /// </summary>
+        public void Reset()
+        {
+            _expand = false;
+            _text = string.Empty;
+        }
+
+        /// <summary>
+        /// 繪製文字區域
+        /// </summary>
+        /// <returns></returns>
+        public void DrawTextFieldIfExpand()
+        {
+            if (!_expand)
+                return;
+
+            _text = EditorGUILayout.TextField(_text);
+        }
+
+        /// <summary>
+        /// 繪製按鈕
+        /// </summary>
+        /// <returns></returns>
+        public bool DrawButtonIfExpand(string text)
+        {
+            if (!_expand)
+                return false;
+
+            return GUILayout.Button(text);
+        }
+    }
+}
