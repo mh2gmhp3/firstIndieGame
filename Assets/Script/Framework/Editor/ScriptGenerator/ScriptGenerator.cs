88e6a8506f706255df82c484d0c4333de1c1aa6f
wei_chen <winnie870206@gmail.com>
2024-10-27 20:56:37 +0800

    feat: ScriptGenerator return Script Object

--- Assets/Script/Framework/Editor/ScriptGenerator/ScriptGenerator.cs
+++ Assets/Script/Framework/Editor/ScriptGenerator/ScriptGenerator.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 using UnityEditor;
 using UnityEngine;
@@ -23,7 +20,7 @@ namespace Framework.Editor.ScriptGenerator
         /// <param name="templatePath">範本路徑</param>
         /// <param name="generatedScriptPath">生成的腳本路徑</param>
         /// <param name="templateReplaceTextList">範本中需要替換的內容</param>
-        public static void GenScript(
+        public static Object GenScript(
             string templatePath,
             string generatedScriptPath,
             List<TemplateReplaceText> templateReplaceTextList)
@@ -31,7 +28,7 @@ namespace Framework.Editor.ScriptGenerator
             if (!File.Exists(templatePath))
             {
                 Debug.LogError("GenScript failed, template file not exist!!");
-                return;
+                return null;
             }
 
             var folderPath = Path.GetDirectoryName(generatedScriptPath);
@@ -49,8 +46,8 @@ namespace Framework.Editor.ScriptGenerator
             }
 
             File.WriteAllText(generatedScriptPath, scriptText, Encoding.UTF8);
-            AssetDatabase.Refresh();
-            AssetDatabase.SaveAssets();
+            AssetDatabase.ImportAsset(generatedScriptPath);
+            return AssetDatabase.LoadAssetAtPath(generatedScriptPath, typeof(Object));
         }
     }
 }
