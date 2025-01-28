a71dc67826d3b44cb9c7e9b019bd666bdd17675b
wei_chen <winnie870206@gmail.com>
2025-01-28 17:02:01 +0800

    feat: UIWidget add WidgetEvent

--- Assets/Script/Framework/UIModule/UIWidget.cs
+++ Assets/Script/Framework/UIModule/UIWidget.cs
@@ -5,10 +5,51 @@ using UnityEngine;
 namespace UIModule
 {
     /// <summary>
+    /// Widget事件資料
+    /// </summary>
+    public struct WidgetEventData
+    {
+        /// <summary>
+        /// 事件Id
+        /// </summary>
+        public int EventId;
+        /// <summary>
+        /// 事件物件(自行轉型)
+        /// </summary>
+        public object Object;
+    }
+
+    public delegate void WidgetEvent(WidgetEventData eventData);
+
+    /// <summary>
     /// UI小型介面
     /// </summary>
     public abstract class UIWidget : UIBase
     {
+        /// <summary>
+        /// Widget事件
+        /// </summary>
+        private WidgetEvent _widgetEvent = null;
 
+        /// <summary>
+        /// 註冊Widget事件
+        /// </summary>
+        /// <param name="widgetEvent"></param>
+        public void RegisterWidgetEvent(WidgetEvent widgetEvent)
+        {
+            _widgetEvent += widgetEvent;
+        }
+
+        /// <summary>
+        /// 呼叫所有註冊事件
+        /// </summary>
+        /// <param name="eventData"></param>
+        public void InvokeWidgetEvent(WidgetEventData eventData)
+        {
+            if (_widgetEvent == null)
+                return;
+
+            _widgetEvent.Invoke(eventData);
+        }
     }
 }
