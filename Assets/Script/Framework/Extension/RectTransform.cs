b950423835f8b1e372d8fd4167f7ba0f328a1266
wei_chen <winnie870206@gmail.com>
2025-01-31 16:35:29 +0800

    feat: add RectTransform Extension

---
+++ Assets/Script/Framework/Extension/RectTransform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Extension
+{
+    public static class RectTransformExtension
+    {
+        /// <summary>
+        /// 設定貼邊延展
+        /// </summary>
+        /// <param name="rectTransform"></param>
+        /// <param name="anchoredPosition"></param>
+        /// <param name="sizeDelta"></param>
+        public static void SetStretch(
+            this RectTransform rectTransform,
+            Vector2 anchoredPosition = default,
+            Vector2 sizeDelta = default)
+        {
+            if (rectTransform == null)
+                return;
+
+            rectTransform.anchorMin = Vector2.zero;
+            rectTransform.anchorMax = Vector2.one;
+            rectTransform.sizeDelta = sizeDelta;
+            rectTransform.anchoredPosition = anchoredPosition;
+        }
+    }
+}
