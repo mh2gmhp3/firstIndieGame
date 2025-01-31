7aeb4b863c2b1abe58e322f55de353119fa6ca2c
wei_chen <winnie870206@gmail.com>
2025-01-31 16:37:00 +0800

    feat: add Widget_Button

---
+++ Assets/Script/Game/UIModule/Script/UIWidget/Widget_Button.InitComponent.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.UI;
+namespace UIModule.Game
+{
+    public partial class Widget_Button : UIWidget
+    {
+        //#REF#
+        private Button Button_Obj;
+        private Text Text_Content;
+        //#REF#
+
+        protected override void InitComponentRefreence()
+        {
+            //#INIT_REF#
+            Button_Obj = _objectReferenceDb.GetObject<Button>("Obj");
+            Text_Content = _objectReferenceDb.GetObject<Text>("Content");
+            //#INIT_REF#
+        }
+
+        protected override void InitComponentEvent()
+        {
+            //#INIT_EVENT#
+            Button_Obj.onClick.AddListener(OnClick_Button_Obj);
+            //#INIT_EVENT#
+        }
+
+        //#EVENT#
+        private void OnClick_Button_Obj()
+        {
+            InvokeWidgetEvent(default);
+        }
+
+        //#EVENT#
+    }
+}
---
+++ Assets/Script/Game/UIModule/Script/UIWidget/Widget_Button.cs
@@ -0,0 +1,11 @@
+﻿namespace UIModule.Game
+{
+    public partial class Widget_Button : UIWidget
+    {
+        public void SetContent(string content)
+        {
+            Init();
+            Text_Content.text = content;
+        }
+    }
+}
