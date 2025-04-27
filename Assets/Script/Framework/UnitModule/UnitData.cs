10cdc1ef7b6d221dde3964f03ba27517eabfa740
wei_chen <winnie870206@gmail.com>
2025-04-27 11:48:49 +0800

    feat: add UnitData  for framework need UnitData

---
+++ Assets/Script/Framework/UnitModule/UnitData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnitModule
+{
+    /// <summary>
+    /// 單位的Collider
+    /// </summary>
+    [Serializable]
+    public class UnitCollider
+    {
+        /// <summary>
+        /// Collider Id
+        /// </summary>
+        public int Id;
+        /// <summary>
+        /// Collider
+        /// </summary>
+        public Collider Collider;
+    }
+
+    /// <summary>
+    /// 單位使用的資料
+    /// </summary>
+    [Serializable]
+    public class UnitData
+    {
+        /// <summary>
+        /// Transform
+        /// </summary>
+        public Transform Transform;
+        /// <summary>
+        /// Animator
+        /// </summary>
+        public Animator Animator;
+        /// <summary>
+        /// Rigidbody
+        /// </summary>
+        public Rigidbody Rigidbody;
+        /// <summary>
+        /// Collider列表
+        /// </summary>
+        public List<UnitCollider> UnitColliderList = new List<UnitCollider>();
+    }
+}
---
+++ Assets/Script/Framework/Utility/UnitUtility.cs
@@ -0,0 +1,39 @@
+﻿using CollisionModule;
+using UnitModule;
+using Logging;
+
+namespace Utility
+{
+    public static class UnitUtility
+    {
+        /// <summary>
+        /// 獲取註冊用碰撞資料
+        /// <para>註冊用資料:<see cref="CollisionAreaManager.RegisterColliderData"/></para>
+        /// <para>註冊使用方法:<see cref="CollisionAreaManager.RegisterCollider(CollisionAreaManager.RegisterColliderData)"/></para>
+        /// </summary>
+        /// <param name="unitData"></param>
+        /// <returns></returns>
+        public static CollisionAreaManager.RegisterColliderData GetColliderData(this UnitData unitData)
+        {
+            var result = new CollisionAreaManager.RegisterColliderData();
+            if (unitData == null)
+            {
+                Log.LogError("UnitUtility.RegisterColliderData Error: unitData is null");
+                return result;
+            }
+
+            for (int i = 0; i < unitData.UnitColliderList.Count; i++)
+            {
+                var unitCollider = unitData.UnitColliderList[i];
+                if (result.IdToRegisterColliderDic.ContainsKey(unitCollider.Id))
+                {
+                    Log.LogWarning($"UnitUtility.RegisterColliderData Error: duplicate id:{unitCollider.Id}");
+                    continue;
+                }
+                result.IdToRegisterColliderDic.Add(unitCollider.Id, unitCollider.Collider);
+            }
+
+            return result;
+        }
+    }
+}
