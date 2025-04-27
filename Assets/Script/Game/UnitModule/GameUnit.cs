9a6f58d0a3c7a9298d03d46be48437a8975296eb
wei_chen <winnie870206@gmail.com>
2025-04-27 11:50:32 +0800

    feat: add GameUnit  for game unit can set unit need component

---
+++ Assets/Script/Game/UnitModule/GameUnit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnitModule
+{
+    /// <summary>
+    /// 角色單位
+    /// </summary>
+    public class GameUnit : MonoBehaviour
+    {
+        public UnitData UnitData;
+    }
+}
