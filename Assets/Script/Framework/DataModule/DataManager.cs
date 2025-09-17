b31033b6b3a3b956516a737c8945172be679617d
wei_chen <winnie870206@gmail.com>
2025-09-17 21:46:16 +0800

    feat: add DataManager can save global and slot save data but only JSON format

---
+++ Assets/Script/Framework/DataModule/DataManager.cs
@@ -0,0 +1,280 @@
+﻿using Logging;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using Utility;
+
+namespace DataModule
+{
+    public class DataManager
+    {
+        private const string DEFAULT_DATA_FOLDER_PREFIX = "data_";
+
+        private string _dataRootFolderPath = string.Empty;
+        private string _dataFolderPrefix = string.Empty;
+
+        private readonly Dictionary<string, IDataRepository> _globalTypeNameToDataRepositoryDic =
+            new Dictionary<string, IDataRepository>();
+        private readonly Dictionary<string, IDataRepository> _slotTypeNameToDataRepositoryDic =
+            new Dictionary<string, IDataRepository>();
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <param name="rootFolderPath"></param>
+        /// <param name="folderPrefix"></param>
+        public void Init(string rootFolderPath = "", string folderPrefix = "")
+        {
+            if (string.IsNullOrEmpty(rootFolderPath))
+                _dataRootFolderPath = Path.Combine(Application.persistentDataPath, "save_data");
+            else
+                _dataRootFolderPath = rootFolderPath;
+            if (string.IsNullOrEmpty(folderPrefix))
+                _dataFolderPrefix = DEFAULT_DATA_FOLDER_PREFIX;
+            else
+                _dataFolderPrefix = folderPrefix;
+            InitRepository();
+        }
+
+        /// <summary>
+        /// 初始化所有資料庫
+        /// </summary>
+        private void InitRepository()
+        {
+            InitGlobalRepository();
+            InitSlotRepository();
+        }
+
+        #region Slot資料庫 Id為單位
+
+        /// <summary>
+        /// 獲取資料庫
+        /// </summary>
+        /// <typeparam name="TDataRepository"></typeparam>
+        /// <returns></returns>
+        public TDataRepository GetDataRepository<TDataRepository>() where TDataRepository : IDataRepository
+        {
+            var type = typeof(TDataRepository);
+            var typeName = type.Name.ToLower();
+            if (!_slotTypeNameToDataRepositoryDic.TryGetValue(typeName, out var result))
+                return default;
+
+            return (TDataRepository)result;
+        }
+
+        /// <summary>
+        /// 保存指定資料
+        /// </summary>
+        /// <param name="id"></param>
+        public void Save(int id)
+        {
+            var dataFolderPath = GetDataFolderPath(id);
+            EnsureFolder(dataFolderPath);
+            foreach (var typeNameToDataRepository in _slotTypeNameToDataRepositoryDic)
+            {
+                var typeName = typeNameToDataRepository.Key;
+                var dataRepository = typeNameToDataRepository.Value;
+
+                dataRepository.Save(dataFolderPath, typeName);
+            }
+        }
+
+        /// <summary>
+        /// 讀取指定資料
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public bool Load(int id)
+        {
+            var dataFolderPath = GetDataFolderPath(id);
+            if (!Directory.Exists(dataFolderPath))
+                return false;
+
+            ClearSlotRepository();
+            foreach (var typeNameToDataRepository in _slotTypeNameToDataRepositoryDic)
+            {
+                var typeName = typeNameToDataRepository.Key;
+                var dataRepository = typeNameToDataRepository.Value;
+
+                dataRepository.Load(dataFolderPath, typeName);
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 刪除指定資料
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public bool Delete(int id)
+        {
+            var dataFolderPath = GetDataFolderPath(id);
+            if (!Directory.Exists(dataFolderPath))
+                return false;
+
+            Directory.Delete(dataFolderPath, true);
+            return true;
+        }
+
+        /// <summary>
+        /// 指定資料是否存在
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public bool Exist(int id)
+        {
+            var dataFolderPath = GetDataFolderPath(id);
+            return Directory.Exists(dataFolderPath);
+        }
+
+        /// <summary>
+        /// 創建新資料
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public void CreateNew(int id)
+        {
+            Delete(id);
+            ClearSlotRepository();
+            Save(id);
+        }
+
+        /// <summary>
+        /// 清空重建Slot資料庫
+        /// </summary>
+        private void ClearSlotRepository()
+        {
+            _slotTypeNameToDataRepositoryDic.Clear();
+            InitSlotRepository();
+        }
+
+        /// <summary>
+        /// 初始化Slot資料庫
+        /// </summary>
+        private void InitSlotRepository()
+        {
+            var typeAndAttributeList = AttributeUtility.GetAllAtttibuteTypeList<DataRepositoryAttribute>();
+            for (int i = 0; i < typeAndAttributeList.Count; i++)
+            {
+                var type = typeAndAttributeList[i].Type;
+                var attribute = typeAndAttributeList[i].Attribute;
+                var typeName = type.Name.ToLower();
+                if (attribute.IsGlobal)
+                    continue;
+
+                if (_slotTypeNameToDataRepositoryDic.ContainsKey(typeName))
+                {
+                    Log.LogError($"DataManger.InitSlotRepository: have duplicate DataRepository name:{typeName}");
+                    continue;
+                }
+
+                var dataRepository = System.Activator.CreateInstance(type, this, attribute.Version) as IDataRepository;
+                _slotTypeNameToDataRepositoryDic.Add(typeName, dataRepository);
+            }
+        }
+
+        #endregion
+
+        #region Global資料庫 全局
+
+        /// <summary>
+        /// 獲取Global資料庫
+        /// </summary>
+        /// <typeparam name="TDataRepository"></typeparam>
+        /// <returns></returns>
+        public TDataRepository GetGlobalDataRepository<TDataRepository>() where TDataRepository : IDataRepository
+        {
+            var type = typeof(TDataRepository);
+            var typeName = type.Name.ToLower();
+            if (!_globalTypeNameToDataRepositoryDic.TryGetValue(typeName, out var result))
+                return default;
+
+            return (TDataRepository)result;
+        }
+
+        /// <summary>
+        /// 保存Global資料
+        /// </summary>
+        /// <param name="id"></param>
+        public void SaveGlobal()
+        {
+            EnsureFolder(_dataRootFolderPath);
+            foreach (var typeNameToDataRepository in _globalTypeNameToDataRepositoryDic)
+            {
+                var typeName = typeNameToDataRepository.Key;
+                var dataRepository = typeNameToDataRepository.Value;
+
+                dataRepository.Save(_dataRootFolderPath, typeName);
+            }
+        }
+
+        /// <summary>
+        /// 讀取Global資料
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public bool LoadGlobal()
+        {
+            if (!Directory.Exists(_dataRootFolderPath))
+                return false;
+
+            foreach (var typeNameToDataRepository in _globalTypeNameToDataRepositoryDic)
+            {
+                var typeName = typeNameToDataRepository.Key;
+                var dataRepository = typeNameToDataRepository.Value;
+
+                dataRepository.Load(_dataRootFolderPath, typeName);
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 初始化Global資料庫
+        /// </summary>
+        private void InitGlobalRepository()
+        {
+            var typeAndAttributeList = AttributeUtility.GetAllAtttibuteTypeList<DataRepositoryAttribute>();
+            for (int i = 0; i < typeAndAttributeList.Count; i++)
+            {
+                var type = typeAndAttributeList[i].Type;
+                var attribute = typeAndAttributeList[i].Attribute;
+                var typeName = type.Name.ToLower();
+                if (!attribute.IsGlobal)
+                    continue;
+
+                if (_globalTypeNameToDataRepositoryDic.ContainsKey(typeName))
+                {
+                    Log.LogError($"DataManger.InitGlobalRepository: have duplicate DataRepository name:{typeName}");
+                    continue;
+                }
+
+                var dataRepository = System.Activator.CreateInstance(type, this, attribute.Version) as IDataRepository;
+                _globalTypeNameToDataRepositoryDic.Add(typeName, dataRepository);
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 確保資料夾
+        /// </summary>
+        /// <param name="path"></param>
+        private void EnsureFolder(string path)
+        {
+            if (Directory.Exists(path))
+                return;
+
+            Directory.CreateDirectory(path);
+        }
+
+        /// <summary>
+        /// 獲取指定檔案資料夾路徑
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        private string GetDataFolderPath(int id)
+        {
+            return Path.Combine(_dataRootFolderPath, _dataFolderPrefix + id.ToString());
+        }
+    }
+}
---
+++ Assets/Script/Framework/DataModule/DataRepository.cs
@@ -0,0 +1,72 @@
+﻿using Logging;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace DataModule
+{
+    public interface IDataRepository
+    {
+        void Save(string dataFolderPath, string name);
+        void Load(string dataFolderPath, string name);
+    }
+
+    public class DataRepositoryAttribute : Attribute
+    {
+        public int Version;
+        public bool IsGlobal;
+
+        public DataRepositoryAttribute(int version, bool isGlobal = false)
+        {
+            Version = version;
+            IsGlobal = isGlobal;
+        }
+    }
+
+    public abstract class DataRepository<TData> : IDataRepository where TData : class, new()
+    {
+        private class SaveData
+        {
+            public int Version;
+            public TData Data;
+        }
+
+        protected DataManager _dataManager;
+        protected TData _data = new TData();
+        private int _version;
+
+        protected DataRepository(DataManager dataManager, int version)
+        {
+            _dataManager = dataManager;
+            _version = version;
+        }
+
+        protected virtual void OnLoad(int currentVersion, int loadedVersion) { }
+
+        void IDataRepository.Save(string dataFolderPath, string name)
+        {
+            var path = Path.Combine(dataFolderPath, name);
+            var saveData = new SaveData
+            {
+                Version = _version,
+                Data = _data
+            };
+            var json = JsonConvert.SerializeObject(saveData);
+            File.WriteAllText(path, json, System.Text.Encoding.UTF8);
+        }
+
+        void IDataRepository.Load(string dataFolderPath, string name)
+        {
+            var path = Path.Combine(dataFolderPath, name);
+            if (!File.Exists(path))
+            {
+                Log.LogWarning($"{this.GetType().Name}.Load, file can not found Path:{path}");
+                return;
+            }
+            var json = File.ReadAllText(path, System.Text.Encoding.UTF8);
+            var saveData = JsonConvert.DeserializeObject<SaveData>(json);
+            _data = saveData.Data;
+            OnLoad(_version, saveData.Version);
+        }
+    }
+}
