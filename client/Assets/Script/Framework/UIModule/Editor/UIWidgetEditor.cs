f3820e6dbfe7858dc000a05cf4ff6c3c401743d5
wei_chen <winnie870206@gmail.com>
2025-10-03 19:10:17 +0800

    feat: add UIWidgetEditor

---
+++ client/Assets/Script/Framework/UIModule/Editor/UIWidgetEditor.cs
@@ -0,0 +1,36 @@
+﻿using Framework.ComponentUtility.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace UIModule
+{
+    [CustomEditor(typeof(UIWidget), true)]
+    public class UIWidgetEditor : Editor
+    {
+        private UIWidget _target = null;
+        private UIWidget Target
+        {
+            get
+            {
+                if (_target == null)
+                    _target = target as UIWidget;
+
+                return _target;
+            }
+        }
+
+        private UIBaseEditorGUI _uiBaseEditorGUI;
+
+        public void OnEnable()
+        {
+            _uiBaseEditorGUI = new UIBaseEditorGUI(Target);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if (GUILayout.Button("開啟編輯視窗"))
+                UIEditorWindow.OpenWindow(Target.gameObject);
+            _uiBaseEditorGUI.OnGUI();
+        }
+    }
+}
