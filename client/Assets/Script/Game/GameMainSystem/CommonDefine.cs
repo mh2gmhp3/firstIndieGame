49bdafe18a3db35c3571e52aca5fbbb29afb2829
wei_chen <winnie870206@gmail.com>
2025-10-13 23:34:50 +0800

    feat: add CommonDefine 共用程式寫死定義

---
+++ client/Assets/Script/Game/GameMainSystem/CommonDefine.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameMainModule
+{
+    public static class CommonDefine
+    {
+        public const int WeaponCount = 3;
+        public const int EmptyWeaponId = 0;
+
+        public const int EmptyAttackBehaviorId = 0;
+    }
+}
