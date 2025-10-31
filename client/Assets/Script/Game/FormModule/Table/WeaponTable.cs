4b901eedc32cda5a8d7346da558129b21f55b158
wei_chen <winnie870206@gmail.com>
2025-10-31 13:45:43 +0800

    feat: WeaponTable add model setup column

--- client/Assets/Script/Game/FormModule/Table/WeaponTable.cs
+++ client/Assets/Script/Game/FormModule/Table/WeaponTable.cs
@@ -7,13 +7,19 @@ namespace FormModule.Game.Table
         public int Id { get; }
         public int Type { get; }
         public int BaseBehaviorCount { get; }
+        public string ModelName { get; }
+        public int UseHand { get; }
+        public int StorageIndex { get; }
 
         [JsonConstructor]
-        public WeaponRow(int id, int type, int baseBehaviorCount)
+        public WeaponRow(int id, int type, int baseBehaviorCount, string modelName, int useHand, int storageIndex)
         {
             Id = id;
             Type = type;
             BaseBehaviorCount = baseBehaviorCount;
+            ModelName = modelName;
+            UseHand = useHand;
+            StorageIndex = storageIndex;
         }
     }
 
