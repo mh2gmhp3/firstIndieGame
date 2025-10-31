10492b31a9fa26bc30b770ed52cabd11f74b6fcb
wei_chen <winnie870206@gmail.com>
2025-10-31 13:44:47 +0800

    feat: add AssetPathUtility to manage AssetPath

---
+++ client/Assets/Script/Game/AssetModule/AssetPathUtility.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace AssetModule
+{
+    public static class AssetPathUtility
+    {
+        private const string WeaponModelFolderPath = "Prototype/TestObject/Weapon";
+
+        public static string GetWeaponModelPath(string modelName)
+        {
+            return Path.Combine(WeaponModelFolderPath, modelName);
+        }
+    }
+}
