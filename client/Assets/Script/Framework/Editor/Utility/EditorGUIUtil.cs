5c1328459a0b3fd651ebe688723851dd3e6a379c
wei_chen <winnie870206@gmail.com>
2025-11-12 19:43:45 +0800

    feat: 加入EditorGUIUtil 加入自訂組合GUI版型

---
+++ client/Assets/Script/Framework/Editor/Utility/EditorGUIUtil.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Framework.Editor.Utility
+{
+    public static class EditorGUIUtil
+    {
+        public static void MinMaxSlider(string label, ref float minValue, ref float maxValue, float minLimit, float maxLimit)
+        {
+            EditorGUILayout.BeginVertical();
+            {
+                EditorGUILayout.BeginHorizontal();
+                {
+                    EditorGUILayout.LabelField(label);
+                    minValue =
+                        Mathf.Clamp(EditorGUILayout.FloatField(minValue), minLimit, maxValue);
+                    maxValue =
+                        Mathf.Clamp(EditorGUILayout.FloatField(maxValue), minValue, maxLimit);
+                }
+                EditorGUILayout.EndHorizontal();
+                EditorGUILayout.MinMaxSlider(
+                    ref minValue, ref maxValue,
+                    minLimit, maxLimit);
+            }
+            EditorGUILayout.EndVertical();
+        }
+    }
+}
