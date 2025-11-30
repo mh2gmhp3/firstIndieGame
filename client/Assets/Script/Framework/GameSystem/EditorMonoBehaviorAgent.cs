f528bd65cf2ecfc3c40ff7a97331bb566cf3a68c
wei_chen <winnie870206@gmail.com>
2025-11-30 12:33:36 +0800

    feat: 新增EditorMonoBehaviorAgent 提供Editor使用的Update代理者

---
+++ client/Assets/Script/Framework/GameSystem/EditorMonoBehaviorAgent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameSystem.RuntimeEditor
+{
+    [ExecuteInEditMode]
+    public class EditorMonoBehaviorAgent : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        private List<IUpdateTarget> _updateTargetList = new List<IUpdateTarget>();
+
+        public void Register(IUpdateTarget target)
+        {
+            if (_updateTargetList.Contains(target))
+                return;
+
+            _updateTargetList.Add(target);
+        }
+
+        public void Unregister(IUpdateTarget target)
+        {
+            _updateTargetList.Remove(target);
+        }
+
+        void Update()
+        {
+            for (int i = 0; i < _updateTargetList.Count; i++)
+            {
+                _updateTargetList[i].DoUpdate();
+            }
+        }
+#endif
+    }
+}
