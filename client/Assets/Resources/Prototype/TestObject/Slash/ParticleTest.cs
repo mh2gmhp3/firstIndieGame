7c13a3f38c28c71b8221032c73d3c2ab4287ac84
wei_chen <winnie870206@gmail.com>
2025-12-12 14:10:53 +0800

    feat: 新增測試Slash特效與設定檔調整

---
+++ client/Assets/Resources/Prototype/TestObject/Slash/ParticleTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+public class ParticleTest : MonoBehaviour
+{
+    public ParticleSystem ParticleSystem;
+    public bool TestEmit;
+    public int EmitCount;
+    public float Duration;
+    public float SpeedRate;
+    public Vector3 Position;
+    public Vector3 Rotation;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TestEmit)
+        {
+            Emit();
+            TestEmit = false;
+        }
+    }
+
+    private void Emit()
+    {
+        if (ParticleSystem == null)
+            return;
+        ParticleSystem.Play();
+    }
+}
